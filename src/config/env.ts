fdcfeaad24d1ad8a7f05e8ace75ee99eb306791e
felipeprsg <felipe.mpersegona@gmail.com>
2023-12-09 16:49:04 -0300

    alterations

--- src/config/env.ts
+++ src/config/env.ts
@@ -11,9 +11,6 @@ const schema = z.object({
     .optional()
     .default('development'),
 
-  // Telegram
-  TELEGRAM_BOT_TOKEN: z.string(),
-
   // Firebase Admin
   FIREBASE_ADMIN_PROJECT_ID: z.string(),
   FIREBASE_ADMIN_CLIENT_EMAIL: z.string(),
@@ -22,7 +19,6 @@ const schema = z.object({
 
 export const env = schema.parse({
   NODE_ENV: process.env.NODE_ENV,
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
   FIREBASE_ADMIN_PROJECT_ID: process.env.FIREBASE_ADMIN_PROJECT_ID,
   FIREBASE_ADMIN_CLIENT_EMAIL: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
   FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
